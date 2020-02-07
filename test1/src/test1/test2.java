eacbb52bceb09eae9e798f62f499b58da9a4818b
Administrator <Administrator@ZJZL-20200112RT>
2020-02-07 15:02:45 +0800

    2

---
+++ test1/src/test1/test2.java
@@ -0,0 +1,11 @@
+package test1;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println(11);
+       System.out.println(12);
+	}
+
+}
